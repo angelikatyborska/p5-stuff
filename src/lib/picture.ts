dd93a13bb2261fc6525eff453d8f52aa04a1c7b3
Angelika Cathor <angelikacathor@fastmail.com>
2025-06-29 15:41:08 +0200

    Add linters

--- src/lib/picture.ts
+++ src/lib/picture.ts
@@ -1,10 +1,13 @@
 import P5 from "p5";
 
-export function picture(Picture: any) {
+export type Work = Pick<P5, "preload" | "setup" | "draw">;
+
+export function picture(Picture: P5) {
   const rootEl = document.getElementById("p5-root");
   if (!rootEl) {
     throw new Error("Cannot find element root #p5-root");
   }
 
+  // @ts-expect-error "is not assignable to parameter of type (...args: any[]) => any"
   new P5(Picture, rootEl);
 }
