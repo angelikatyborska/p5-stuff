1116a8cf3f62d259a3e13b6ab1eee88e2fc1c12a
Angelika Cathor <angelikacathor@fastmail.com>
2025-07-26 19:51:12 +0200

    Setup CI

---
+++ bin/verify_og_images.cjs
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+const slugs = fs.readdirSync("src/pages/works/");
+const paths = [
+  ...slugs.map((slug) => `public/og/${slug.split('.')[0]}.png`),
+];
+
+const missingImages = paths.filter((path) => {
+  return !fs.statSync(path, { throwIfNoEntry: false });
+});
+
+if (missingImages.length > 0) {
+  console.log(`Missing OG images:\n${missingImages.join("\n")}`);
+  process.exit(1);
+} else {
+  console.log("all ok");
+}
